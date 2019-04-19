9be963b6a1331e142e28dec3ef0993c22e5800e8
JuanCRodriguez <Rodriguez.JuanCarlos@Outlook.com>
2019-04-19 19:04:35 -0300

    fingirAmor

--- TP 0/main.hs
+++ TP 0/main.hs
@@ -1,27 +1,25 @@
 type TCoche = (String, Double, Int, String)
-data DCoche = DCoche TCoche
 --coches
-rayo = DCoche ("RochaMcQueen", 300, 0 , "Ro")
-biankerr = DCoche ("Biankerr", 500, 20, "Tinch")
-gushtav = DCoche ("Gushtav", 200, 130, "PetiLaLinda")
-rodra = DCoche ("Rodra", 0, 50, "Taisa")
+rayo = ("RochaMcQueen", 300.0, 0 , "Ro")
+biankerr = ("Biankerr", 500.0, 20, "Tinch")
+gushtav = ("Gushtav", 200.0, 130, "PetiLaLinda")
+rodra = ("Rodra", 0, 50.0, "Taisa")
 
 --gets
-getName (DCoche (name, _ , _,_)) = name
-getNafta (DCoche (_, nafta, _,_)) = nafta
-getVelocidad (DCoche (_, _, velocidad, _)) = velocidad
-getEnamorado (DCoche (_, _, _, enamorado)) = enamorado
-getEnamoradoCantChars = length . getEnamorado
-getCantIncrVelocidad = cantIncrVelocidad . getEnamoradoCantChars
-
-
+getName (name, _ , _,_) = name
+getNafta (_, nafta, _,_) = nafta
+getVelocidad (_, _, velocidad, _) = velocidad
+getEnamorado (_, _, _, enamorado) = enamorado
+getEnamoradoCantChars coche = (length) (getEnamorado coche) 
+getCantIncrVelocidad coche = (cantIncrVelocidad) (getEnamoradoCantChars coche)
 
 cantIncrVelocidad x | x<=2 = 15
                     | x<=5 = 20
                     | otherwise = 30
+
+
 deReversa = ((+) (1000 * 0.2)) . getNafta
 impresionar = ((*) 2) . getVelocidad
 nitro = ((+) 15) . getVelocidad
 incrementarVelocidad coche = (+) (getCantIncrVelocidad coche) (getVelocidad coche)
-
---TODO: fingirAmor
+fingirAmor (a, b , c, d) r = getEnamorado((a, b, c, r))
