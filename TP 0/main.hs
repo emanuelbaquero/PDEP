bddc3be086cc7d47473d593ddb89483617fe6d2d
JuanCRodriguez <Rodriguez.JuanCarlos@Outlook.com>
2019-04-19 23:47:38 -0300

    v3

--- TP 0/main.hs
+++ TP 0/main.hs
@@ -1,36 +1,39 @@
 type TCoche = ([Char], Double, Integer, [Char]) --(Nombre, Nafta, Velocidad, Enamorado)
-getName :: TCoche -> String
-getNafta :: TCoche -> Double
-getVelocidad :: TCoche -> Integer
-getEnamorado :: TCoche -> String
-getEnamoradoCantChars  :: TCoche -> Int
-getCantIncrVelocidad :: TCoche -> Integer
+name :: TCoche -> String
+nafta :: TCoche -> Double
+velocidad :: TCoche -> Integer
+enamorado :: TCoche -> String
+enamoradoCantChars  :: TCoche -> Int
+cantIncrVelocidad :: TCoche -> Integer
+criterioVelocidad :: (Num d, Ord a, Num a) => a -> d
 deReversa :: TCoche -> Double
 impresionar :: TCoche -> Integer
 incrementarVelocidad :: TCoche -> Integer
-fingirAmor :: TCoche -> String -> String
+fingirAmor :: TCoche -> String -> TCoche
+mostrarAmorFingido :: TCoche -> String -> String
 
 --coches
 rayo = ("RochaMcQueen", 300.0, 0 , "Ro")
 biankerr = ("Biankerr", 500.0, 20, "Tinch")
 gushtav = ("Gushtav", 200.0, 130, "PetiLaLinda")
-rodra = ("Rodra", 0, 50.0, "Taisa")
+rodra = ("Rodra", 0.0, 50, "Taisa")
 
 --gets
-getName (name, _ , _,_) = name
-getNafta (_, nafta, _,_) = nafta
-getVelocidad (_, _, velocidad, _) = velocidad
-getEnamorado (_, _, _, enamorado) = enamorado
-getEnamoradoCantChars coche = (length) (getEnamorado coche) 
-getCantIncrVelocidad coche = (cantIncrVelocidad) (getEnamoradoCantChars coche)
+name (name, _ , _,_) = name
+nafta (_, nafta, _,_) = nafta
+velocidad (_, _, velocidad, _) = velocidad
+enamorado (_, _, _, enamorado) = enamorado
+enamoradoCantChars coche = (length) (enamorado coche) 
+cantIncrVelocidad coche = (criterioVelocidad) (enamoradoCantChars coche)
 
-cantIncrVelocidad x | x<=2 = 15
+criterioVelocidad x | x<=2 = 15
                     | x<=5 = 20
                     | otherwise = 30
 
 
-deReversa = ((+) (1000 * 0.2)) . getNafta
-impresionar = ((*) 2) . getVelocidad
-nitro = ((+) 15) . getVelocidad
-incrementarVelocidad coche = (+) (getCantIncrVelocidad coche) (getVelocidad coche)
-fingirAmor (a, b , c, d) r = getEnamorado((a, b, c, r))
+deReversa = ((+) (1000 * 0.2)) . nafta
+impresionar = ((*) 2) . velocidad
+nitro = ((+) 15) . velocidad
+incrementarVelocidad coche = (+) (cantIncrVelocidad coche) (velocidad coche)
+fingirAmor (a, b , c, d) r =(a, b, c, r)
+mostrarAmorFingido coche nuevoEnamorado = (enamorado . fingirAmor coche) nuevoEnamorado 
